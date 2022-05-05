c78898c7644dbcd8187e5019f9682831a7ec8849
Matthew Jurenka <matt.jurenka@comcast.net>
2022-05-05 16:34:02 -0700

    updated fuzz test to new impl structure

--- src/rust/fuzz/fuzz_targets/read.rs
+++ src/rust/fuzz/fuzz_targets/read.rs
@@ -1,15 +1,22 @@
 #![no_main]
 
-use flatgeobuf::*;
 use libfuzzer_sys::fuzz_target;
+use flatgeobuf::*;
+use flatgeobuf::FgbReader;
 use std::io;
+
 
 fuzz_target!(|data: &[u8]| {
     let mut buf_reader = io::BufReader::new(io::Cursor::new(data));
-    let mut fgb = match FgbReader::open(&mut buf_reader) {
+    let fgb = match FgbReader::open(&mut buf_reader) {
+        Ok(n) => n,
+        Err(_) => return,
+    };
+    let mut fgb = match fgb.select_all() {
         Ok(n) => n,
         Err(_) => return,
     };
     let _ = fgb.header();
+    //fgb should be Result<FgbReader<'a, R, Open>>
     while let Ok(Some(_feature)) = fgb.next() {}
 });
