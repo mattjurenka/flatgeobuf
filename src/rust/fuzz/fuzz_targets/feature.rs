75bdf60bee23de2081153f02aca436865c25c2d3
Matthew Jurenka <matt.jurenka@comcast.net>
2022-05-06 10:40:48 -0700

    fixed feature fuzzer

--- src/rust/fuzz/fuzz_targets/feature.rs
+++ src/rust/fuzz/fuzz_targets/feature.rs
@@ -4,7 +4,10 @@ use flatgeobuf::*;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let feature = root_as_feature(data).unwrap();
+    let feature = match root_as_feature(data) {
+        Ok(feature) => feature,
+        Err(_) => return,
+    };
     let _ = feature.geometry();
     let _ = match feature.properties() {
         Some(n) => n,
